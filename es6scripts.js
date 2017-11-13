e7138286a8341ba24b783edbc514682027ae625b
Óli P :) <olafur.palsson2@gmail.com>
2017-11-13 15:15:48 +0000

    Bjó til function til að búa til video

--- es6scripts.js
+++ es6scripts.js
@@ -1,2 +1,60 @@
-console.log("hello world")
-console.log("that's bullshit")
+(() => {
+  const makeVideoCard = (videoID) => {
+    const card    = document.createElement("div")
+    const video   = document.createElement("video")
+    const heading = document.createElement("h2")
+    const text    = document.createElement("p")
+
+    card    .classList.add("cards__singleCard")
+    video   .classList.add("cards__singleCard__video")
+    heading .classList.add("cards__singleCard__heading")
+    text    .classList.add("cards__singleCard__time")
+
+    let timeText    = getFormattedTime(videoID)
+    let headingText = getHeading(videoID)
+
+    timeText    = document.createTextNode(timeText)
+    headingText = document.createTextNode(headingText)
+
+    heading.appendChild(headingText)
+    text   .appendChild(timeText)
+
+    card.appendChild(video)    
+    card.appendChild(heading)
+    card.appendChild(text)
+
+    return card
+  }
+
+  const getFormattedTime = (videoID) => {
+    const time = getTime(videoID)
+
+    //format shit
+
+    return time
+  }
+
+  const getTime = (videoID) => {
+    //const blabla = videoList.time
+    const blabla = "eitthvað"
+    return blabla
+  }
+
+  const getHeading = (videoID) => {
+    return "Súrt dæmi"
+  }
+
+  const getJSON = () => {
+    const reader = new FileReader()
+  }
+
+  //test
+  (() => {
+    document.querySelector(".body").appendChild(makeVideoCard(1))
+  })()
+
+
+
+
+
+})()
